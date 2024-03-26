498fdeb76bc152be4de4445731c96ab2c00ae54f
fardosa001 <fardosaadow@gmail.com>
2024-03-26 12:12:57 +0000

    Top Students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,14 +5,13 @@
 def top_students(mongo_collection):
     """returns all students sorted by average score"""
     pipeline = [
-        {
-            "$project":
-            {
-                "name": "$name",
-                "averageScore": {"avg": "$scores.score"}
-            }
-        },
-        {"$sort": {"averageScore": -1}}
+        {"$unwind": "$scores"},
+        {"$group": {
+            "_id": "$_id",
+            "averageScore": {"$avg": "$scores.score"},
+            "name": {"$first": "$name"}  # Assuming student's name is stored in a field named "name"
+        }},
+        {"$sort": {"averageScore": -1}}  # Sort by average score in descending order
     ]
 
     result = mongo_collection.aggregate(pipeline)
